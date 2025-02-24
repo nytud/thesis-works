6c79910a52d2d41933c9d77819e7f4182db05a53
Boglárka Skrabák <skrabakbogi@gmail.com>
2025-02-24 14:45:25 +0100

    Sort every sentence in a separate sublist with head-token and rawhead-token tuples

--- launcher_huspacy_emagyar/packages/launcher_emagyar.py
+++ launcher_huspacy_emagyar/packages/launcher_emagyar.py
@@ -99,6 +99,7 @@ def makelists(fname, e_tokens, e_lems, e_morph, e_ners, e_only_ners, e_pos, e_de
                 e_dep.append(str(splitline[10]))
                 e_head_num.append(str(splitline[11]))
                 ids.append((splitline[9], splitline[0]))
+                print("token:", splitline[0], "dep:", splitline[10], "head_num:", splitline[11], "id:", splitline[9])
                 
                 
                 if(splitline[13] != 'O'): #ner conversion: emagyar works with a different iob label set
@@ -132,7 +133,8 @@ def makelists(fname, e_tokens, e_lems, e_morph, e_ners, e_only_ners, e_pos, e_de
 def make_head_list(ids, e_head_num, e_dep, e_head, e_tokens):
     #sorts the ids into separate lists per sentences
     ids_per_sentences = list([])
-    i = -1
+    i = -1 #always append to the end
+    #i = 0
 
     for (id, tok) in ids:
         if(id == '1'):#new sentence begins
@@ -140,8 +142,9 @@ def make_head_list(ids, e_head_num, e_dep, e_head, e_tokens):
             ids_per_sentences.append(l)
             i += 1
         ids_per_sentences[i].append((int(id), tok))#we append a token and an id so we can map that back easily (each token with its own id)
+        print("ids_per_sentences:", ids_per_sentences)
 
-    i = 0#which sentence (sublist) are we in
+    """i = 0#which sentence (sublist) are we in
     move = False#should we switch sentence (sublist)
     for j in range(0, len(e_head_num)):#iterating through the raw headlist
         h = int(e_head_num[j])#get the raw head
@@ -149,19 +152,38 @@ def make_head_list(ids, e_head_num, e_dep, e_head, e_tokens):
         if(move and h == 0 and i < len(ids_per_sentences)-1):#we are at the second zero, aka the . (note: emagyar can make mistakes (on specifiy cases) in this, so that's not 100% certain)
             i += 1
             move = False
+            print("moved")
         
         if(e_dep[j] == 'ROOT'):
             move = True#if root found, we have to move when the next 0 hits
+            print("ROOT found")
             #note: somehow only the sentence terminating puntcuation mark gets 0, so other punctuation chars get decent phrase ids
         if(h == 0):
             e_head.append(e_tokens[j])
+            print("headnum is 0", e_tokens[j])
             #e_head.append("")
         else:
             for (id, tok) in ids_per_sentences[i]:
                 if(h == id):
                     e_head.append(tok)
-                    #print(i, tok)
-                    break
+                    print("tok:", tok, "id:", id, "h:", h)
+                    break"""
+    
+
+    raw_head_tok = list(zip(e_head_num, e_tokens))
+    split_rht = list([])
+    prev_len = 0
+
+    for sent in ids_per_sentences:
+        curr_len = len(sent)
+        print(prev_len, curr_len, prev_len + curr_len)
+        print(raw_head_tok[prev_len:(prev_len + curr_len)])
+        split_rht.append(raw_head_tok[prev_len:(prev_len + curr_len)])
+        prev_len += curr_len
+
+    
+
+
 
 
 
