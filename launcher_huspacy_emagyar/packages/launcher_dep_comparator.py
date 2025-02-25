3b283a57035ad526313e0d238f59363c3155ff56
Boglárka Skrabák <skrabakbogi@gmail.com>
2025-02-25 15:10:20 +0100

    Fix refactoring bug in dep comp

--- launcher_huspacy_emagyar/packages/launcher_dep_comparator.py
+++ launcher_huspacy_emagyar/packages/launcher_dep_comparator.py
@@ -45,6 +45,9 @@ def dep_comparator(h_dep, h_head, e_dep, e_head, h_tokens, e_tokens):
                     j, k, m = diffsolver(z, v, j, k, h_tokens, e_tokens, str_to_print, diff_to_print_e, diff_to_print_h, h_dep, h_head, None, e_dep, e_head)
                     if(m):
                         break
+                    j, k, m = diffsolver(v, z, j, k, h_tokens, e_tokens, str_to_print, diff_to_print_e, diff_to_print_h, h_dep, h_head, None, e_dep, e_head)
+                    if(m):
+                        break
             if(m):
                 continue
             
